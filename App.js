3c2eda192d92050aed9c74e24987cc2750b90e3f
AilenMz <ailen.martinez1995@gmail.com>
2022-02-14 15:03:02 -0300

    prueba

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Bienvenidos a MarvelAPP!</Text>
+      <Text>Bienvenidos a MarvelApp!</Text>
       <StatusBar style="auto" />
     </View>
   );
